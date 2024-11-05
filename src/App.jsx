0a07daf3d60108eacb6bc918f38e02427ff232af
ezgi-karaca <w.ezgikaraca@gmail.com>
2024-11-05 15:00:28 +0300

    Pages eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
-import Footer from './pages/footer'
-import Header from './pages/header'
-import Main from './pages/main'
-import Profile from './pages/profile'
-import Projects from './pages/projects'
-import Footer from './pages/footer'
-import Skills from './pages/skills'
+import Footer from './pages/Footer.jsx';
+import Header from './pages/Header';
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Main/>
+      <Skills/>
+      <Profile/>
+      <Projects/>
+      <Footer/>
     </>
   )
 }
