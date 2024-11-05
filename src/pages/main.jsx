323e62a0083e0f077fd7467a254ca94995f377ab
ezgi-karaca <w.ezgikaraca@gmail.com>
2024-11-05 20:27:36 +0300

    FontAwesome ile ikonlar eklendi

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ezgi from '../assets/ezgi.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Main = () => {
   return (
@@ -16,8 +18,8 @@ const Main = () => {
         <p className='text-lg max-w-lg'>Hi, I’m Ezgi. I’m a full-stack developer. If you are looking for a Developer who to craft solid and scalable frontend products with great user experiences. Let’s shake hands with me.</p>
         <div className='flex space-x-4'>
           <button className='px-6 py-2 bg-white border-2 border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition'>Hire me</button>
-          <button className='px-6 py-2 bg-white border-2 border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition'><span></span>Github</button>
-          <button className='px-6 py-2 bg-white border-2 border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition'><span></span>LinkedIn</button>
+          <button className='px-6 py-2 bg-white border-2 border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition'><FontAwesomeIcon icon={faGithub} className="mr-2" />Github</button>
+          <button className='px-6 py-2 bg-white border-2 border-purple-500 text-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition'><FontAwesomeIcon icon={faLinkedin} className="mr-2" />LinkedIn</button>
         </div>
       </div>
       <img className="w-1/3 rounded-lg mt-10" src={ezgi}/>
