d9f5d67692dd987296c3d0604e9bcd4db843f892
ezgi-karaca <w.ezgikaraca@gmail.com>
2024-11-05 15:24:48 +0300

    header icerikleri

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,8 +1,16 @@
 
-
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="left">
+        <button className="menu-icon">☰</button>
+      </div>
+      <div className="right">
+        <a href="#home" className="link">Skills</a>
+        <a href="#about" className="link">Projects</a>
+        <a href="#contact" className="link">Hire me</a>
+      </div>
+    </header>
   )
 }
 
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <p className="w-32 h-px bg-[#3730A3]">-</p>
+      <h6>Ezgi Karaca</h6>
+    </div>
   )
 }
 
